97c835b445ff2cd7f71d7e5f701fb2f7a4e07a70
FLAVIO SANTARELLI <145938765+PROFSANTARELLI@users.noreply.github.com>
2025-04-14 18:20:12 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_utils",
+    version="0.1.0",
+    packages=find_packages(),
+    description="Funções matemáticas básicas",
+    author="Seu Nome",
+    author_email="seu@email.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+)
